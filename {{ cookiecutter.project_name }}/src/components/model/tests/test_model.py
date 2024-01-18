96277d34ac2a7dd19c3d2e62d7cd07b190558b88
maranedah <mauricio.araneda@ug.uchile.cl>
2024-01-18 13:05:56 -0300

    cookiecutter changes

--- {{ cookiecutter.project_name }}/src/components/model/tests/test_model.py
+++ {{ cookiecutter.project_name }}/src/components/model/tests/test_model.py
@@ -7,17 +7,15 @@ from test_utils import SklearnModelMock
 
 class TestModel(unittest.TestCase):
     def setUp(self):
-        self.project_id = "ml-projects-399119"
-        self.preprocessed_dataset_uri = "gs://pipeline-template-dev/preprocessed.gzip"
-        self.predictions_uri = "gs://pipeline-template-dev/predictions.gzip"
+        self.project_id = "{{ cookiecutter.project_id }}"
+        self.dataset_uri = "{{ cookiecutter.gcs_bucket }}-test/preprocessed.gzip"
         self.model_uri = ""
         self.metrics_uri = ""
 
     @patch.object("lightgbm.LGBMRegressor", SklearnModelMock)
     def test_run_model(self):
-        (df, model, metrics) = run_model(
+        model, metrics = run_model(
             self.project_id,
             self.preprocessed_dataset_uri,
         )
-        df.to_parquet(self.predictions_uri)
-        assert df.shape[0] > 0
+        assert isinstance(metrics, dict)
