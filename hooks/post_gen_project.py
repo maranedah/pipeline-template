7c70e74e727b7d5f851d277769cb326b7c3de2e4
maranedah <mauricio.araneda@ug.uchile.cl>
2024-02-11 13:16:54 -0700

    post gen python

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+
+def run_terraform_apply():
+    try:
+        # Change directory to 'terraform'
+        os.chdir("terraform")
+
+        # Run 'terraform init' command
+        subprocess.run(["terraform", "init"], check=True)
+
+        # Run 'terraform apply -auto-approve' command
+        subprocess.run(["terraform", "apply", "-auto-approve"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running Terraform commands: {e}")
+        print(f"Command output:\n{e.output.decode()}")
+
+
+if __name__ == "__main__":
+    run_terraform_apply()
