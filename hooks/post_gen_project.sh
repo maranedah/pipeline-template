06a0452efd38a73baca580914d8b2dfb6af74d2c
maranedah <mauricio.araneda@ug.uchile.cl>
2024-02-11 13:10:25 -0700

    post gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,4 +5,6 @@ echo "Running post-generation script..."
 # Add your post-generation actions here
 # ...
 
+cd terraform && terraform init && terraform apply
+
 echo "Post-generation script completed."
