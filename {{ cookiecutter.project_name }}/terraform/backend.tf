40eadc983fa708c4484263356dfdc1dab6cfeb38
maranedah <mauricio.araneda@ug.uchile.cl>
2024-02-19 13:24:17 -0700

    terraform

--- {{ cookiecutter.project_name }}/terraform/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-backend "gcs" {
-  bucket = "{{ cookiecutter.project_id }}-tfstate-bucket"   # GCS bucket name to store terraform tfstate
-  prefix = {{ cookiecutter.project_name }}
-  }
-}
