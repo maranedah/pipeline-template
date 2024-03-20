8ddbffd46176b6cd848bc05ad056361fca36401d
maranedah <mauricio.araneda@ug.uchile.cl>
2024-03-20 05:25:45 -0700

    ensemble optimization

--- {{ cookiecutter.project_name }}/src/components/model/src/model/models/regression_models.py
+++ {{ cookiecutter.project_name }}/src/components/model/src/model/models/regression_models.py
@@ -1,20 +1,16 @@
 import logging
+from datetime import datetime, timezone
 
 import lightgbm as lgb
 import mlflow
 import numpy as np
+import optuna
 import pandas as pd
 import xgboost as xgb
 from catboost import CatBoostRegressor
 from lightgbm import LGBMRegressor
 from model.hyperparameter_tuning import HyperparameterTuning
-from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
-from sklearn.linear_model import ElasticNet, Lasso, LinearRegression, Ridge
 from sklearn.model_selection import KFold, cross_validate
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.neural_network import MLPRegressor
-from sklearn.svm import SVR
-from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
 
 logging.basicConfig(
@@ -25,16 +21,16 @@ logging.basicConfig(
 class RegressionModels:
     def __init__(self):
         self.models = [
-            LinearRegression,
-            Ridge,
-            Lasso,
-            ElasticNet,
-            DecisionTreeRegressor,
-            RandomForestRegressor,
-            GradientBoostingRegressor,
-            SVR,
-            KNeighborsRegressor,
-            MLPRegressor,
+            # LinearRegression,
+            # Ridge,
+            # Lasso,
+            # ElasticNet,
+            # DecisionTreeRegressor,
+            # RandomForestRegressor,
+            # GradientBoostingRegressor,
+            # SVR,
+            # KNeighborsRegressor,
+            # MLPRegressor,
             XGBRegressor,
             LGBMRegressor,
             CatBoostRegressor,
@@ -48,128 +44,169 @@ class RegressionModels:
         ]
         self.tuning_metric = "neg_mean_absolute_percentage_error"
         self.cv_splits = 5
+        self.suggestion_trials = None
+        self.timeout_in_hours = 3
 
-    def cross_validation(self, models, X, y, eval_set):
-        logging.info("Cross Validation...")
-        cv = KFold(n_splits=self.cv_splits, shuffle=True, random_state=42)
-        cv_info = []
-        for model in models:
-            logging.info(f"Training model {type(model).__name__}")
-            result = cross_validate(
-                estimator=model,
-                X=X,
-                y=y,
-                scoring=self.metrics,
-                cv=cv,
-                params=self.get_fit_params(
-                    type(model), eval_set, early_stopping_rounds=5
-                ),
-                n_jobs=-1,
-            )
-            cv_info.append(result)
-
-        logging.info("Finished Cross Validation...")
-        return cv_info
-
-    def fit(self, X, y, eval_set):
-        # Init all models with default params
+    def init_models(self):
         logging.info("Instantiating models with default parameters")
-        models = [model(**self.get_init_params(model)) for model in self.models]
+        return [model(**self.get_init_params(model)) for model in self.models]
 
-        # Cross Validation for each default model
-        cv_models_info = self.cross_validation(models, X, y, eval_set)
-
-        models_performance = [
-            {
-                "model_name": type(model).__name__,
-                **{k: v.mean() for k, v in cv_info.items()},
-            }
-            for model, cv_info in zip(models, cv_models_info)
+    def get_scores_from_mlflow(self, experiment_timestamp):
+        runs_df = mlflow.search_runs()
+        time_filter = runs_df.start_time.dt.tz_convert(None) > experiment_timestamp
+        runs_df = runs_df[time_filter]
+        metrics_cols = [col for col in runs_df.columns if col.startswith("metrics.")]
+        renaming = [
+            col.split(".")[-1] for col in runs_df.columns if col.startswith("metrics.")
         ]
-        mlflow.set_experiment("Model Selection - No Tuning")
+        models_score = runs_df[metrics_cols].rename(
+            columns=dict(zip(metrics_cols, renaming))
+        )
+        return models_score
 
-        for model_performance in models_performance:
+    def score_models(self, models, X, y, eval_set):
+        mlflow.set_experiment("All Models")
+
+        experiment_timestamp = np.datetime64(datetime.now(timezone.utc), "ns")
+
+        cv = KFold(n_splits=self.cv_splits, shuffle=True, random_state=42)
+        for model in models:
             with mlflow.start_run():
-                for k, v in model_performance.items():
-                    if k == "model_name":
-                        mlflow.log_param("model_name", v)
-                    else:
-                        mlflow.log_metric(k, v)
+                logging.info(f"Training model {type(model).__name__}")
+                result = cross_validate(
+                    estimator=model,
+                    X=X,
+                    y=y,
+                    scoring=self.metrics,
+                    cv=cv,
+                    params=self.get_fit_params(type(model), eval_set),
+                    n_jobs=-1,
+                )
+                mlflow.log_param("model", type(model).__name__)
+                for k, v in result.items():
+                    mlflow.log_metric(k, v.mean())
 
-        df = pd.DataFrame(models_performance)
-        # Get top 3 models
-        top_3_indexes = df.nlargest(3, f"test_{self.tuning_metric}").index.tolist()
-        top_3_models = [self.models[i] for i in top_3_indexes]
+        logging.info("Finished Training...")
+        models_score = self.get_scores_from_mlflow(experiment_timestamp)
 
-        # Hyperparameter tuning of best 3 models
-        suggestion_trials = None
-        timeout_in_hours = 0.5
-        early_stopping_rounds = 5
-        model_studies = [
-            HyperparameterTuning(
+        return models_score
+
+    def get_top_k_models(self, df_models_score, top_k):
+        top_k_models = (
+            df_models_score.nlargest(n=top_k, columns=f"test_{self.tuning_metric}")
+            .index.map(self.models.__getitem__)
+            .tolist()
+        )
+        return top_k_models
+
+    def best_models_hparams_tuning(self, best_models, eval_set):
+        mlflow.set_experiment("All Models")
+        studies = []
+        scores = []
+        for model in best_models:
+            study = HyperparameterTuning(
                 model,
                 metrics=self.tuning_metric,
                 model_init_params=self.get_init_params(model),
             ).run_tuning(
                 X,
                 y,
-                self.get_fit_params(model, eval_set, early_stopping_rounds),
+                self.get_fit_params(model, eval_set),
                 self.cv_splits,
-                suggestion_trials,
-                timeout_in_hours,
+                self.suggestion_trials,
+                self.timeout_in_hours,
             )
-            for model in top_3_models
-        ]
+            scores.append(study.best_value)
+            studies.append(study)
+        models_score = pd.DataFrame(scores, columns=["score"])
+        return models_score, studies
 
-        # Assign optimized top 3 best models to object
-        self.top_3_models = [
+    def init_tuned_models(self, best_models, studies, eval_set):
+        best_models = [
             model(**study.best_params, **self.get_init_params(model)).fit(
-                X, y, **self.get_fit_params(model, eval_set, early_stopping_rounds)
+                X, y, **self.get_fit_params(model, eval_set)
             )
-            for study, model in zip(model_studies, top_3_models)
+            for study, model in zip(studies, best_models)
+        ]
+        return best_models
+
+    def ensemble_tuning(self, eval_set):
+        X_val, Y_val = eval_set
+        logging.info("Ensemble tuning")
+
+        def objective(trial):
+            # Define the search space for parameters
+            a = trial.suggest_float("a", 0.0, 1.0)
+            b = trial.suggest_float("b", 0.0, 1.0 - a)
+            c = 1 - a - b
+
+            # Example objective function (can be replaced with any other function)
+            predictions = [
+                model.predict(X_val) * coefficient
+                for model, coefficient in zip(self.best_models, [a, b, c])
+            ]
+            predictions = np.array(predictions).sum(axis=0)
+            score = mean_absolute_percentage_error(Y_val, predictions)
+            return score
+
+        # Perform optimization
+        study = optuna.create_study(direction="minimize")
+        study.optimize(objective, n_trials=1000)
+        best_params = study.best_params
+        self.mix_coefficients = [
+            best_params["a"],
+            best_params["b"],
+            1 - best_params["a"] - best_params["b"],
         ]
 
-        # Save top 3 models results
-        top_3_performances = [
-            {"model_name": type(model()).__name__, self.tuning_metric: study.best_value}
-            for study, model in zip(model_studies, top_3_models)
-        ]
-        top_3_df = pd.DataFrame(top_3_performances)
-        top_3_df.to_csv("Top 3 Tuned Model Performances.csv", index=False)
+    def fit(self, X, y, eval_set):
+        models = self.init_models()
+        df_models_score = self.score_models(models, X, y, eval_set)
+        best_models = self.get_top_k_models(df_models_score, top_k=3)
+        top_models_score, studies = self.best_models_hparams_tuning(
+            best_models, eval_set
+        )
+        self.best_models = self.init_tuned_models(best_models, studies, eval_set)
 
+        self.ensemble_tuning(eval_set)
         # Get best model
-        self.best_model = self.top_3_models[
-            top_3_df.nlargest(1, self.tuning_metric).index.tolist()[0]
+        self.best_model = self.best_models[
+            top_models_score.nsmallest(1, "score").index.tolist()[0]
         ]
 
     def predict(self, X):
         return self.best_model.predict(X)
 
     def predict_ensemble(self, X):
-        return np.array([model.predict(X) for model in self.top_3_models]).mean(axis=0)
+        return np.array(
+            [
+                model.predict(X) * coef
+                for model, coef in zip(self.best_models, self.mix_coefficients)
+            ]
+        ).sum(axis=0)
 
-    def get_fit_params(self, model, eval_set, early_stopping_rounds):
+    def predict_ensemble_mean(self, X):
+        return np.array([model.predict(X) for model in self.best_models]).mean(axis=0)
+
+    def get_fit_params(self, model, eval_set):
         model_name = type(model()).__name__
         fit_params = {
             "LGBMRegressor": {
                 "eval_set": eval_set,
                 "eval_metric": "mape",
-                "callbacks": [
-                    lgb.early_stopping(
-                        stopping_rounds=early_stopping_rounds, verbose=False
-                    )
-                ],
+                "callbacks": [lgb.early_stopping(stopping_rounds=5, verbose=False)],
             },
             "CatBoostRegressor": {
                 "eval_set": eval_set,
                 "verbose": 0,
                 # TODO: find how to add mape metric to catboost
-                "early_stopping_rounds": early_stopping_rounds,
+                "early_stopping_rounds": 5,
                 "callbacks": [],
             },
             "XGBRegressor": {
                 "eval_set": [eval_set],
                 "verbose": 0,
+                "callbacks": [xgb.callback.EarlyStopping(rounds=5)],
             },
         }
         if model_name in fit_params:
@@ -191,7 +228,6 @@ class RegressionModels:
                 "random_seed": 42,
                 "verbose_eval": False,
                 "eval_metric": "mape",
-                "callbacks": [xgb.callback.EarlyStopping(rounds=5)],
             },
         }
         if model_name in init_params:
@@ -217,4 +253,5 @@ if __name__ == "__main__":
     )
     model.fit(X_train, y_train, eval_set=(X_val, y_val))
     print(mean_absolute_percentage_error(y_test, model.predict(X_test)))
+    print(mean_absolute_percentage_error(y_test, model.predict_ensemble_mean(X_test)))
     print(mean_absolute_percentage_error(y_test, model.predict_ensemble(X_test)))
